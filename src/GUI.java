24ff9da9d2dff76e772b6aafce20e12a8088a7ad
nowlan <anthony.nowlan@internode.on.net>
2013-10-15 15:07:57 +1100

    Added Class headers
    http://swinbrain.ict.swin.edu.au/wiki/Swinburne_Java_Coding_Standard

--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,23 @@
+/**    
+ * A concise description of the class (including invariants if any)
+ *    
+ * @
+ * PUBLIC FEATURES:
+ * // Constructors
+ *    each constructor should be listed here
+ * // Methods
+ *    The signature and a brief comment (if needed)
+ *    In alphabetic order
+ *
+ * COLLABORATORS:
+ *    Names of classes (other than System and java.lang)
+ *
+ * MODIFIED:
+ * @version number, date last changed and author’s initials &/or what changed (very brief)
+ * @author    (can have multiple authors)
+ * http://swinbrain.ict.swin.edu.au/wiki/Swinburne_Java_Coding_Standard
+ */
+ 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
--- src/VolaTile.java
+++ src/VolaTile.java
@@ -1,3 +1,22 @@
+/**    
+ * A concise description of the class (including invariants if any)
+ *    
+ * @
+ * PUBLIC FEATURES:
+ * // Constructors
+ *    each constructor should be listed here
+ * // Methods
+ *    The signature and a brief comment (if needed)
+ *    In alphabetic order
+ *
+ * COLLABORATORS:
+ *    Names of classes (other than System and java.lang)
+ *
+ * MODIFIED:
+ * @version number, date last changed and author’s initials &/or what changed (very brief)
+ * @author    (can have multiple authors)
+ */
+ 
 import java.io.IOException;
 
 public class VolaTile{
