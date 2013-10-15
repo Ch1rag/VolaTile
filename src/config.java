b28e5d93938cd3b78f7a40d5b2fb383bb5e5e461
nowlan <anthony.nowlan@internode.on.net>
2013-10-15 16:15:54 +1100

    Config class added synchronize double check
    Double check that instance is still null after synchronizing thread.

--- src/config.java
+++ src/config.java
@@ -2,14 +2,13 @@
 public class Config
 {
 	// alternatively, we can eagerly create instance 
-	private static Config uniqueInstance;
+	private volatile static Config uniqueInstance;
 	private String volatilityPath;
 	private String pythonPath;
 	
 	private Config()
 	{
 		
-	
 		// should read these strings from file.
 		volatilityPath	= "c:\\vol\\vol.py";
 		pythonPath 		= "c:\\python\\python";
@@ -17,11 +16,17 @@ public class Config
 	}
 	
 	// sychronize force every thread to wait its turn before entering.
-	public static synchronized Config getInstance()	
+	public static Config getInstance()	
 	{
-		if (uniqueInstance == null)
+		if (uniqueInstance == null)		// check if instance already running.
 		{
-			uniqueInstance = new Config();
+			synchronized (Config.class)	// if not synchronize threads first time through.
+			{
+				if (uniqueInstance == null)	// double check that still null inside sync block
+				{
+					uniqueInstance = new Config();	// create a new instance of class.
+				}
+			}
 		}
 		return uniqueInstance;
 	}
